134b96559e34320665ba3cb4978aaf8ca5f5e074
Corea <coreawalter249@gmail.com>
2026-08-11 15:44:03 -0600

    Se agrega la clase Libro

--- src/Libro.java
+++ src/Libro.java
@@ -8,5 +8,68 @@
  * @author MSI
  */
 public class Libro {
+    private String idLibro;
+    private String titulo;
+    private String autor;
+    private String editorial;
+    private int anioPublicacion;
+    private boolean disponible;
+
+    public Libro(String idLibro, String titulo, String autor, String editorial, int anioPublicacion, boolean disponible) {
+        this.idLibro = idLibro;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editorial = editorial;
+        this.anioPublicacion = anioPublicacion;
+        this.disponible = disponible;
+    }
+
+    public String getIdLibro() {
+        return idLibro;
+    }
+
+    public void setIdLibro(String idLibro) {
+        this.idLibro = idLibro;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    public void setAnioPublicacion(int anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
     
 }
